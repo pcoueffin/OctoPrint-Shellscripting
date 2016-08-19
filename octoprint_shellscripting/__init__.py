e086c53b4afded8a617f8ed44eef8560d27b6ea4
pcoueffin <pcoueffin+githubstuff@gmail.com>
2016-08-19 07:45:40 -0700

    Added a settings page.

--- octoprint_shellscripting/__init__.py
+++ octoprint_shellscripting/__init__.py
@@ -4,11 +4,23 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class ShellscriptingPlugin(octoprint.plugin.StartupPlugin,
-                           octoprint.plugin.TemplatePlugin):
+                           octoprint.plugin.TemplatePlugin,
+                           octoprint.plugin.SettingsPlugin):
 
-    def on_after_startup(self):
-        self._logger.info("Hello World!")
+        def on_after_startup(self):
+                self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
 
+        def get_settings_defaults(self):
+                return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+        def get_template_configs(self):
+                return [
+                        dict(type="navbar", custom_bindings=False),
+                        dict(type="settings", custom_bindings=False)
+                ]
+
+
+__plugin_name__ = "Shell Scripting"
 __plugin_implementation__ = ShellscriptingPlugin()
 
 
